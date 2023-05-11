45d0f716c0eecd034458a101735745a8b94c2efe
Johnson Mao <tutelary.maomao@gmail.com>
2023-05-11 22:49:45 +0800

    feat: button

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <h1>遊戲大廳！</h1>
-      <Button></Button >
+      <Button>進入</Button>
     </>
   );
 }
--- shared/components/Button/index.tsx
+++ shared/components/Button/index.tsx
@@ -1,3 +1,5 @@
-export default function Button() {
-  return <button>Button</button>;
-}
+import Button from "./Button";
+
+export * from "./Button";
+
+export default Button;
