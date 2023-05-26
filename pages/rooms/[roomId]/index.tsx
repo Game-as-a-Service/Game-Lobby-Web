440e56a4f0258c9f5287ffb82bf08d4ed9e99f20
yuwen <kfk07027@gmail.com>
2023-05-27 00:36:05 +0800

    feat: apply useRoom

--- pages/rooms/[roomId]/index.tsx
+++ pages/rooms/[roomId]/index.tsx
@@ -1,24 +1,112 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Button from "@/shared/components/Button";
 import UserCard, { UserCardProps } from "./components/UserCard";
+import useRequest from "@/shared/hooks/useRequest";
+import useRoom from "@/shared/hooks/context/useRoom";
+import { getRoomInfoEndpoint } from "@/requests/rooms";
+import { RoomInfoType } from "@/shared/containers/provider/RoomProvider/type";
 
 const SEAT_AMOUNT = 10;
 
 export default function Room() {
-  function renderUserCards(users: UserCardProps[]) {
+  const {
+    roomInfo,
+    initialize,
+    addPlayer,
+    removePlayer,
+    updateHost,
+    updateRoomStatus,
+    toggleUserReadyStatus,
+    cleanRoom,
+  } = useRoom();
+  const { fetch } = useRequest();
+  const { query } = useRouter();
+  const roomId = query.roomId as string;
+
+  useEffect(() => {
+    async function getRoomInfo() {
+      const roomInfo = (await fetch(
+        getRoomInfoEndpoint(roomId)
+      )) as any as RoomInfoType;
+      initialize(roomInfo);
+    }
+
+    getRoomInfo();
+  }, [fetch, initialize, roomId]);
+
+  // mock behavior start
+  useEffect(() => {
+    setTimeout(() => {
+      addPlayer({ id: "bcd", nickname: "第二位進來啦" });
+    }, 2000);
+
+    setTimeout(() => {
+      addPlayer({ id: "bcde", nickname: "第三位進來啦" });
+    }, 3000);
+  }, [addPlayer]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      updateHost("bcd");
+    }, 4000);
+  }, [updateHost]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      updateRoomStatus("PLAYING");
+    }, 5000);
+  }, [updateRoomStatus]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      removePlayer("bcd");
+    }, 6000);
+  }, [removePlayer]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      toggleUserReadyStatus("abc");
+    }, 7000);
+  }, [toggleUserReadyStatus]);
+
+  useEffect(() => {
+    setTimeout(() => cleanRoom(), 8000);
+  }, [cleanRoom]);
+  // mock behavior end
+
+  function renderUserCards(users: RoomInfoType["players"]) {
     const userCount = users.length;
-    const userCards = users.map((user, index) => (
-      <UserCard key={user.userName || index} {...user} />
-    ));
-    const disabledCards = Array.from({ length: SEAT_AMOUNT - userCount }).map(
-      (_, index) => <UserCard key={userCount + index} disabled />
-    );
-    return [...userCards, ...disabledCards];
+
+    const userCards = users.map((user) => {
+      const props: UserCardProps = {
+        nickName: user.nickname,
+        isReady: user.isReady,
+        // TODO: 假設使用者本人的 id 為 abc
+        isSelf: user.id === "abc",
+        isHost: user.id === roomInfo.host.id,
+      };
+      return <UserCard key={user.id} {...props} />;
+    });
+
+    // render rest seats
+    const emptyCards = Array.from({
+      length: SEAT_AMOUNT - userCount,
+    }).map((_, index) => {
+      // render wating seat
+      if (userCount + index < roomInfo.maxPlayers)
+        return <UserCard key={userCount + index} isWating />;
+      // render disabled seat
+      return <UserCard key={userCount + index} disabled />;
+    });
+
+    return [...userCards, ...emptyCards];
   }
 
   return (
     <div className="p-[18px] flex flex-col gap-[36px] max-w-[1172px] bg-[#212123]">
       <div className="userList grid grid-cols-5 gap-x-5 gap-y-[60px]">
-        {renderUserCards(mockUsers)}
+        {renderUserCards(roomInfo.players)}
       </div>
       <div className="flex items-center">
         <div className="grow min-w-[643px] border self-stretch text-center text-white">
@@ -45,25 +133,3 @@ export default function Room() {
     </div>
   );
 }
-
-const mockUsers: UserCardProps[] = [
-  {
-    userName: "abc",
-    isReady: false,
-    isHost: true,
-    isSelf: true,
-  },
-  {
-    userName: "abcd",
-    isReady: false,
-    isHost: false,
-    isSelf: false,
-  },
-  {
-    userName: "abcde",
-    isReady: true,
-    isHost: false,
-    isSelf: false,
-  },
-  {},
-];
