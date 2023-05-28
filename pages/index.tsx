4ff48e96bc6dac02fd30a902439b924bd7b6b731
yuwen <kfk07027@gmail.com>
2023-05-28 18:09:46 +0800

    fix: remove unuse code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,10 @@
 import { GetStaticProps } from "next";
-import Button from "@/shared/components/Button";
-import { useState, useContext } from "react";
-import Modalow from "@/shared/components/Modalow";
 import CreateRoomModal from "./components/CreateGameRoom/CreateRoomModal";
-import CreateGameRoomContext from "@/shared/contexts/CreateRoomContext";
-const mockUserSelectGameData = {
-  gameId: "string2",
-  minPlayers: 3,
-  maxPlayers: 7,
-  displayName: "string",
-};
 
 export default function Home() {
-  const [show, setShow] = useState(false);
-  const { roomForm, setRoomForm } = useContext(CreateGameRoomContext);
-  function handleClickCreateRoom() {
-    setRoomForm({
-      ...roomForm,
-      gameId: mockUserSelectGameData.gameId,
-      minPlayers: mockUserSelectGameData.minPlayers,
-      maxPlayers: mockUserSelectGameData.maxPlayers,
-    });
-    setShow(true);
-  }
   return (
     <>
       <h1>遊戲大廳！</h1>
-
       <CreateRoomModal />
     </>
   );
