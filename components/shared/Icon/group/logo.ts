cb1f4cb02ec341311d2869c9a41657a5f0e4c1cc
Johnson Mao <tutelary.maomao@gmail.com>
2023-06-02 23:06:31 +0800

    feat: 💄 add login layout
    Delete login_cover.svg

---
+++ components/shared/Icon/group/logo.ts
@@ -0,0 +1,28 @@
+import { IconVariants } from "../types/types";
+
+export const logo: IconVariants = {
+  name: "logo",
+  definition: {
+    svg: {
+      viewBox: "0 0 79 79",
+      width: 79,
+      height: 79,
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg",
+    },
+    path: [
+      {
+        d: "M1.35646e-05 39.5C1.54717e-05 17.6847 17.6848 4.75104e-06 39.5 6.65819e-06C51.2977 7.68958e-06 79 1.01114e-05 79 1.01114e-05C79 1.01114e-05 79 29.4824 79 39.5C79 61.3153 61.3153 79 39.5 79C17.6848 79 1.16574e-05 61.3153 1.35646e-05 39.5Z",
+        fill: "#2F88FF",
+      },
+      {
+        d: "M48.5491 23.4377H61.942V50.2236H48.5491V23.4377Z",
+        fill: "white",
+      },
+      {
+        d: "M20.0898 23.4377H33.4828V50.2236H20.0898V23.4377Z",
+        fill: "white",
+      },
+    ],
+  },
+};
