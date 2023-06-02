5493d66aa113ecf794dfc1d375d2f57c34b861ed
kylemocode <kylemo860617@gmail.com>
2023-06-03 00:07:11 +0800

    👷 complete knip workflow octokit client

--- scripts/knipScanReporter.js
+++ scripts/knipScanReporter.js
@@ -3,14 +3,17 @@ const path = require("path");
 const readline = require("readline");
 const { Octokit } = require("@octokit/core");
 const { context } = require("@actions/github");
-console.log("context  ", context);
+
 const knipScanResultPath = path.join(process.cwd(), "knipScanResult.txt");
 
 const HOST_NAME = "https://github.com/";
-// const octokit = new Octokit({
-//   auth: process.env.GIT_TOKEN,
-//   baseUrl: `${HOST_NAME}/api/v3`,
-// });
+const octokit = new Octokit({
+  auth: process.env.GIT_TOKEN,
+  baseUrl: `${HOST_NAME}/api/v3`,
+});
+
+const REPO_FULL_NAME = context.payload.repository.full_name;
+const PR_NUMBER = context.payload.pull_request.number;
 
 const categories = [
   "Unused files",
@@ -61,10 +64,6 @@ rl.on("close", () => {
 });
 
 const addOrUpdateComment = (resultSections) => {
-  // const DRONE_REPO = process.env.REPO_NAME;
-  // const DRONE_PULL_REQUEST = process.env.PULL_REQUEST;
-  // const DRONE_COMMIT_SHA = process.env.COMMIT_SHA;
-
   let commentBody = `Knip Scan Result \n\n`;
 
   for (const [section, lines] of Object.entries(resultSections)) {
@@ -72,27 +71,27 @@ const addOrUpdateComment = (resultSections) => {
     commentBody += lines.join("\n") + "\n</details>\n\n";
   }
 
-  // octokit
-  //   .request(`GET /repos/${DRONE_REPO}/issues/${DRONE_PULL_REQUEST}/comments`)
-  //   .then((response) => {
-  //     const comments = response.data;
+  octokit
+    .request(`GET /repos/${REPO_FULL_NAME}/issues/${PR_NUMBER}/comments`)
+    .then((response) => {
+      const comments = response.data;
 
-  //     return comments.find((comment) => commentRegex.test(comment.body));
-  //   })
-  //   .then((knipScanComment) => {
-  //     if (knipScanComment) {
-  //       return octokit.request(
-  //         `DELETE /repos/${DRONE_REPO}/issues/comments/${knipScanComment.id}`
-  //       );
-  //     }
-  //   })
-  //   .then(() => {
-  //     return octokit.request(
-  //       `POST /repos/${DRONE_REPO}/issues/${DRONE_PULL_REQUEST}/comments`,
-  //       {
-  //         body: commentBody,
-  //       }
-  //     );
-  //   })
-  //   .catch(console.error);
+      return comments.find((comment) => commentRegex.test(comment.body));
+    })
+    .then((knipScanComment) => {
+      if (knipScanComment) {
+        return octokit.request(
+          `DELETE /repos/${REPO_FULL_NAME}/issues/comments/${knipScanComment.id}`
+        );
+      }
+    })
+    .then(() => {
+      return octokit.request(
+        `POST /repos/${REPO_FULL_NAME}/issues/${PR_NUMBER}/comments`,
+        {
+          body: commentBody,
+        }
+      );
+    })
+    .catch(console.error);
 };
