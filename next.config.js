77bcb4094b89f3ffc554574aa44a5a5d143fcb0c
kylemocode <kylemo860617@gmail.com>
2023-06-04 23:46:36 +0800

    🐛 fix production docker file build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+let withBundleAnalyzer = (config) => config;
+
+if (process.env.ANALYZE === "true") {
+  withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: true,
+  });
+}
 
 const nextConfig = {
   eslint: {
