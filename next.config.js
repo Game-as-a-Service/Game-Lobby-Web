3f054d1719ca80503dc012857c07023b15c34ea8
kylemocode <kylemo860617@gmail.com>
2023-06-04 23:50:09 +0800

    🔥 chore: clean comment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
 
 let withBundleAnalyzer = (config) => config;
 
