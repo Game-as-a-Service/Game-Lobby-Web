f8939f5177ce4cb77d923a8021ee12570881531d
kylemocode <kylemo860617@gmail.com>
2023-06-08 16:17:48 +0800

    🔧 add bundlewatch in CI

---
+++ .bundlewatch.config.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+const root = "./.next/static/chunks/";
+
+const files = fs.readdirSync(root);
+
+const checkList = [
+  // page _app
+  {
+    path: "./.next/static/chunks/pages/_app*.js",
+    maxSize: "180KB",
+  },
+  // other pagesxw
+  {
+    path: "./.next/static/chunks/pages/**/!(_app)*.js",
+    maxSize: "50KB",
+  },
+  // CSS
+  {
+    path: "./.next/static/css/*.css",
+    maxSize: "15KB",
+  },
+];
+
+files.forEach((file) => {
+  if (file.replace(/\..{16}\.js$/, "").match(/^(?=.*\d).{8}$/)) {
+    // node modules
+    checkList.push({
+      path: root + file,
+      maxSize: "120KB",
+    });
+  } else if (file.match(/\.js$/)) {
+    // other chunks
+    checkList.push({
+      path: root + file,
+      maxSize: "50KB",
+    });
+  }
+});
+
+const bundleWatchConfig = {
+  files: checkList,
+};
+
+module.exports = bundleWatchConfig;
