89de5de47e57a97c5f6962ccb1b718f7db4a5bdc
Fiona Ye <bxbdev2022@gmail.com>
2023-06-26 22:02:45 +0800

    feat: ready room

--- pages/api/mock/rooms/[roomId]/players/[userId].ts
+++ pages/api/mock/rooms/[roomId]/players/[userId].ts
@@ -8,4 +8,16 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       return res.end();
     }
   }
+
+  if (req.method === "POST") {
+    const userId = req.query.userId;
+    const roomId = req.query.roomId;
+    if (roomId && userId === "me:ready") {
+      return res.json({ message: "Success" });
+    }
+
+    if (roomId && userId === "me:cancel") {
+      return res.json({ message: "Success" });
+    }
+  }
 }
