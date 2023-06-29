e997d0534b6a1b67215404ce5a5270502696acd0
Johnson Mao <tutelary.maomao@gmail.com>
2023-06-29 22:30:46 +0800

    ✨ feat: enter room

---
+++ pages/api/mock/rooms/[roomId]/players/index.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { wait } from "@/lib/utils";
+import { mock_rooms } from "@/mocks/room";
+import { RoomType } from "@/requests/rooms";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { roomId } = req.query;
+    const data = mock_rooms(RoomType.WAITING).find(
+      (room) => room.id === roomId
+    );
+
+    await wait(800);
+
+    if (data?.isLocked && req.body.password !== "1234") {
+      return res.status(400).json({ message: "wrong password" });
+    }
+
+    return res.json({ message: "success" });
+  }
+}
