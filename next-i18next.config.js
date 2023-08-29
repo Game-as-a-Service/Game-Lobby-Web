824a9c82d616cfbbf98766e71ed51ac3c077bb14
Johnson Mao <tutelary.maomao@gmail.com>
2023-08-29 20:49:40 +0800

    🐛 fix i18n not working in production issue

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require("path");
+
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: "zh-TW",
     locales: ["zh-TW"],
   },
+  localePath: path.resolve("./public/locales"),
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
