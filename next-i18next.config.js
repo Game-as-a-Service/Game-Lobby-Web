59b48f7f4eaa60c27622a5c792fc2e588c9b9d43
Johnson Mao <tutelary.maomao@gmail.com>
2023-08-30 22:20:45 +0800

    🐛 fix i18n production not match json file

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,12 @@
-const path = require("path");
-
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: "zh-TW",
     locales: ["zh-TW"],
   },
-  localePath: path.resolve("../public/locales"),
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/locales",
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
