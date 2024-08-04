11ebba6634da84b86cc55552449b17a89e0f95c5
Johnson Mao <tutelary.maomao@gmail.com>
2024-08-04 16:42:37 +0800

    feat: carousel v2 transition animation

--- components/shared/Carousel/v2/Carousel.tsx
+++ components/shared/Carousel/v2/Carousel.tsx
@@ -1,22 +1,23 @@
-import { FC, useState } from "react";
+import { CSSProperties, FC, useEffect, useRef, useState } from "react";
 import Icon from "../../Icon/v2/Icon";
-import { cn } from "@/lib/utils";
 
 interface CarouselProps<
-  T extends Record<string, unknown>,
-  K extends keyof T = keyof T,
+  Item extends Record<string, unknown>,
+  Key extends keyof Item = keyof Item,
 > {
-  items: (T & Record<K, string>)[];
-  uniqueKey: K;
-  Component: FC<T>;
+  items: (Item & Record<Key, string>)[];
+  uniqueKey: Key;
+  Component: FC<Item>;
 }
 
-export default function Carousel<T extends Record<string, unknown>>({
+export default function Carousel<Item extends Record<string, unknown>>({
   items,
   uniqueKey,
   Component,
-}: Readonly<CarouselProps<T>>) {
+}: Readonly<CarouselProps<Item>>) {
   const [showIndex, setShowIndex] = useState(0);
+  const [maxWidth, setMaxWidth] = useState(0);
+  const carouselRef = useRef<HTMLDivElement>(null);
 
   const handleChangePage = (action: "prev" | "next") => () => {
     const maxIndex = items.length - 1;
@@ -42,6 +43,10 @@ export default function Carousel<T extends Record<string, unknown>>({
     "p-2.5 shrink-0 bg-white/4 shadow-default rounded-2xl";
   const buttonIconClassName = "stroke-white w-6 h-6 pointer-events-none";
 
+  useEffect(() => {
+    setMaxWidth(carouselRef.current?.clientWidth || 0);
+  }, []);
+
   return (
     <div className="flex items-center">
       <button
@@ -51,17 +56,31 @@ export default function Carousel<T extends Record<string, unknown>>({
       >
         <Icon name="navArrowLeft" className={buttonIconClassName} />
       </button>
-      <ul>
-        {Array.isArray(items) &&
-          items.map((item, index) => (
-            <li
-              key={item[uniqueKey]}
-              className={cn(showIndex !== index && "hidden")}
-            >
-              <Component {...item} />
-            </li>
-          ))}
-      </ul>
+      <div ref={carouselRef} className="overflow-hidden w-full">
+        <ul
+          className="flex transition-transform translate-x-[var(--translate-x)]"
+          style={
+            {
+              "--translate-x": `${showIndex * maxWidth * -1}px`,
+            } as CSSProperties
+          }
+        >
+          {Array.isArray(items) &&
+            items.map((item) => (
+              <li
+                key={item[uniqueKey]}
+                className="shrink-0 w-[var(--max-width)]"
+                style={
+                  {
+                    "--max-width": `${maxWidth}px`,
+                  } as CSSProperties
+                }
+              >
+                <Component {...item} />
+              </li>
+            ))}
+        </ul>
+      </div>
       <button
         type="button"
         className={buttonClassName}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,55 +6,85 @@ import { useTranslation } from "next-i18next";
 
 import Button from "@/components/shared/Button";
 import CreateRoomModal from "@/components/lobby/CreateRoomModal";
-import Carousel, { mockCarouselItems } from "@/components/shared/Carousel";
+import { mockCarouselItems } from "@/components/shared/Carousel";
 import CarouselV2 from "@/components/shared/Carousel/v2";
 import FastJoinButton from "@/components/lobby/FastJoinButton";
 import SearchBar from "@/components/shared/SearchBar";
 
-function CarouselCard({ imgUrl, imgAlt }: (typeof mockCarouselItems)[number]) {
+function CarouselCard({
+  imgUrl,
+  imgAlt,
+}: Readonly<(typeof mockCarouselItems)[number]>) {
   return (
-    <div className="flex text-white px-10 gap-4">
+    <div className="flex text-white px-12 gap-4">
       <div className="relative flex-[60%]">
-        <Image
-          src={imgUrl}
-          alt={imgAlt ? imgAlt : ""}
-          draggable={false}
-          priority
-          fill
-        />
+        <Image src={imgUrl} alt={imgAlt} draggable={false} priority fill />
       </div>
       <div className="flex-[40%] p-4 rounded-lg bg-primary-50/8">
-        <div className="text-xs">Massive Monster</div>
-        <div className="text-xl">AZUL ({imgAlt})</div>
-        <div className="flex mb-2 text-xs">
-          <div className="flex-1">4.6 * * * * * (14)</div>
+        <div className="text-xs text-primary-300">Massive Monster</div>
+        <div className="text-xl text-primary-100">AZUL ({imgAlt})</div>
+        <div className="flex mb-2 text-xs text-primary-300">
+          <div className="flex-1">4.6 ＊ ＊ ＊ ＊ ＊ (14)</div>
           <time className="flex-1">2023.08.25</time>
         </div>
         <div className="mb-3">
           <ul className="flex gap-3">
-            <li className="border border-primary-100 w-16 h-10">img1</li>
-            <li className="border border-primary-100 w-16 h-10">img2</li>
-            <li className="border border-primary-100 w-16 h-10">img3</li>
+            <li className="relative w-16 h-10">
+              <Image
+                src="https://images.unsplash.com/photo-1533237264985-ee62f6d342bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+                alt={imgAlt}
+                draggable={false}
+                priority
+                fill
+              />
+            </li>
+            <li className="relative w-16 h-10">
+              <Image
+                src="https://images.unsplash.com/photo-1613160717888-faa82cdb8a94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                alt={imgAlt}
+                draggable={false}
+                priority
+                fill
+              />
+            </li>
+            <li className="relative w-16 h-10">
+              <Image
+                src="https://images.unsplash.com/photo-1601987177651-8edfe6c20009?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                alt={imgAlt}
+                draggable={false}
+                priority
+                fill
+              />
+            </li>
+            <li className="relative w-16 h-10">
+              <Image
+                src="https://images.unsplash.com/photo-1511512578047-dfb367046420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+                alt={imgAlt}
+                draggable={false}
+                priority
+                fill
+              />
+            </li>
           </ul>
         </div>
-        <div className="mb-3 text-xs">
+        <div className="mb-3 text-xs text-primary-50">
           《AZUL》是強手棋類休閒遊戲，在遊戲中，你可以任意選擇比賽地圖、參賽角色和組隊方式，使用卡片等方式賺取金錢，最終取得比賽勝利。
         </div>
         <div>
           <ul className="flex flex-wrap gap-2 text-xs">
-            <li className="border border-primary-100 rounded-full px-1">
+            <li className="bg-primary-800 text-primary-300 rounded-full px-2 py-0.5">
               回合制
             </li>
-            <li className="border border-primary-100 rounded-full px-1">
+            <li className="bg-primary-800 text-primary-300 rounded-full px-2 py-0.5">
               第三人稱
             </li>
-            <li className="border border-primary-100 rounded-full px-1">
+            <li className="bg-primary-800 text-primary-300 rounded-full px-2 py-0.5">
               策略型
             </li>
-            <li className="border border-primary-100 rounded-full px-1">
+            <li className="bg-primary-800 text-primary-300 rounded-full px-2 py-0.5">
               玩家對戰
             </li>
-            <li className="border border-primary-100 rounded-full px-1">
+            <li className="bg-primary-800 text-primary-300 rounded-full px-2 py-0.5">
               輕鬆休閒
             </li>
           </ul>
@@ -84,20 +114,13 @@ export default function Home() {
           Component={CarouselCard}
         />
       </div>
-      <div className="mt-[12px] mb-[22px]">
-        <Carousel
-          itemWidth={332}
-          itemHeight={158}
-          items={mockCarouselItems}
-          itemsToSlide={2}
-          autoplay
-        />
+      <div className="my-6 flex gap-3">
+        <CreateRoomModal />
+        <Button component={Link} href="/rooms">
+          {t("rooms_list")}
+        </Button>
+        <FastJoinButton />
       </div>
-      <CreateRoomModal />
-      <Button component={Link} href="/rooms">
-        {t("rooms_list")}
-      </Button>
-      <FastJoinButton />
     </div>
   );
 }
