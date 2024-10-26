6c135281a4eada2d4c9b36e8a0f780da321432e5
Johnson Mao <tutelary.maomao@gmail.com>
2024-10-26 16:33:39 +0800

    feat: auto reset hook

---
+++ hooks/useAutoReset.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+function useAutoReset<T>(initialValue: T, resetDelayMs = 1000) {
+  const [internalValue, setInternalValue] = useState(initialValue);
+  const timerRef = useRef<NodeJS.Timeout>();
+
+  const clearTimer = useCallback(() => {
+    clearTimeout(timerRef.current);
+  }, []);
+
+  const setValue = (newValue: T) => {
+    setInternalValue(newValue);
+    clearTimer();
+    timerRef.current = setTimeout(
+      () => setInternalValue(initialValue),
+      resetDelayMs
+    );
+  };
+
+  useEffect(() => {
+    return () => clearTimer();
+  }, [clearTimer]);
+
+  return [internalValue, setValue] as const;
+}
+
+export default useAutoReset;
