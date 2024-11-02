b7ae8b3c33f3d8d6d55017715fb5b33b577cb97f
Johnson Mao <tutelary.maomao@gmail.com>
2024-11-02 20:12:45 +0800

    feat(Cover): handle error to display fallback image

--- components/shared/Cover/Cover.stories.tsx
+++ components/shared/Cover/Cover.stories.tsx
@@ -4,59 +4,35 @@ import Cover from "./index";
 const meta: Meta<typeof Cover> = {
   title: "Data Display/ Cover",
   component: Cover,
-  decorators: [
-    (Story) => (
-      <div className="flex gap-2 items-center w-fit">
-        <Story />
-      </div>
-    ),
-  ],
   tags: ["autodocs"],
   args: {
     src: "https://images.unsplash.com/photo-1684006231760-3f0290bf42a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&q=80",
     alt: "Cover",
+    width: 100,
+    height: 130,
   },
   argTypes: {
-    src: { control: "text" },
-    alt: { control: "text" },
-    width: { control: "number" },
-    height: { control: "number" },
-    fill: { control: "boolean" },
-    className: { control: "text" },
+    src: {
+      type: "string",
+    },
+    alt: {
+      type: "string",
+    },
   },
 };
 
 type Story = StoryObj<typeof Cover>;
 
-export const Playgrond: Story = {
-  render: (args) => {
-    return <Cover {...args} />;
-  },
+export const Playground: Story = {
+  render: (args) => <Cover {...args} />,
 };
 
-export const CoverFill: Story = {
-  args: {
-    src: "https://images.unsplash.com/photo-1682687219573-3fd75f982217?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&q=80",
-    alt: "Cover2",
-    fill: true,
-    className: "w-[250px] h-[250px]",
-  },
-  render: (args) => {
-    return <Cover {...args} />;
-  },
+export const DefaultErrorCover: Story = {
+  render: (args) => <Cover {...args} />,
 };
 
-export const CoverWithVewport: Story = {
-  args: {
-    src: "https://images.unsplash.com/photo-1685350854473-3f7822f1633a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
-    alt: "Cover1",
-    width: 100,
-    height: 100,
-    className: "w-full sm:w-[100%] md:w-[320px] lg:w-[150px]",
-  },
-  render: (args) => {
-    return <Cover {...args} />;
-  },
+DefaultErrorCover.args = {
+  src: "error src",
 };
 
 export default meta;
--- components/shared/Cover/cover.test.tsx
+++ components/shared/Cover/cover.test.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import Cover from ".";
 
 describe("Cover", () => {
-  const image = {
-    src: "https://images.unsplash.com/photo-1683125695370-1c7fc9ff1315?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160",
-    alt: "Cover1",
-  };
-
-  test("should render", () => {
-    render(<Cover {...image} />);
-    const _img = document.querySelector("img");
-    expect(_img).toBeInTheDocument();
-    expect(_img?.alt).toBe("Cover1");
-    expect(_img?.src).toMatch(/photo-1683125695370-1c7fc9ff1315/i);
+  test("should render Cover component", () => {
+    render(
+      <Cover
+        src="https://images.unsplash.com/photo-1683125695370-1c7fc9ff1315?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160"
+        alt="Cover"
+        width={300}
+        height={300}
+      />
+    );
+    const cover = screen.getByRole("img");
+    expect(cover).toBeInTheDocument();
+    expect(cover).toHaveAccessibleName("Cover");
   });
 });
